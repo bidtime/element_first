e9ce60461085adf1f627170d7d638d99067b9f41
riverbo <riverbo@126.com>
2017-12-07 18:03:36 +0800

    新增页面

---
+++ src/brand/addBrand.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+<el-form :model="frmInsert">
+        <el-form-item label="carBrandName">
+            <el-input v-model="frmInsert.carBrandName"></el-input>
+        </el-form-item>
+        <el-form-item label="carBrandCode">
+            <el-input v-model="frmInsert.carBrandCode"></el-input>
+        </el-form-item>
+    </el-form>
+    <el-button @click="save">提交</el-button>
+</div>
+    
+</template>
+<script>
+import axios from 'axios';
+export default {
+    data(){
+        return {
+            frmInsert:{
+                carBrandName:'',
+                carBrandCode:''
+            }
+        }
+    },
+    methods:{
+        save(){
+            axios.post('',this.frmInsert).then(res=>{
+
+            }).catch(error=>{
+
+            })
+        }
+    }
+
+}
+
+</script>
--- src/brand/brand_list.vue
+++ src/brand/brand_list.vue
@@ -1,20 +1,28 @@
 <template>
 
-    <div>  
+    <div> 
+    <el-input v-model="carBrandName" size="large"/>
+    <el-button @click="query">search</el-button>
+    <el-button @click="show=true">insert</el-button>
+    
     <el-table :data="data" border>
-        <el-table-column label="carBrand">
-            <el-table-column label="carBrandName" prop="carBrandName"></el-table-column>
-            <el-table-column label="stateName" prop="stateName"></el-table-column>
+        <el-table-column label="carBrand" align="center">
+            <el-table-column label="carBrandName" prop="carBrandName" align="left"></el-table-column>
+            <el-table-column label="stateName" prop="stateName" align="left"></el-table-column>
+            <el-table-column label="createTime" prop="createTime" align="left"></el-table-column>
+            <el-table-column label="carBrandCode" prop="carBrandCode" align="left"></el-table-column>
         </el-table-column>
     </el-table>
-    <el-input v-model="brandName"></el-input>
-    <el-button @click="query">search</el-button>
+    <el-dialog title="插入品牌" :visible.sync="show">
+        <addBrand></addBrand>
+    </el-dialog>
     </div> 
 
 </template>    
 
 <script>
 import axios from 'axios';
+import addBrand from './addBrand.vue';
 export default {
     data(){
         return {
@@ -23,7 +31,8 @@ export default {
                     
                 }
             ],
-            brandName:''
+            carBrandName:'',
+            show:false,
         }
     },
 
@@ -31,7 +40,7 @@ export default {
         query(){
             axios.get('/api/ap/car/brand/list',{
                 params:{
-                    brandName: this.brandName
+                    carBrandName: this.carBrandName
                 }
             }).then(res=>{
                 if(res.data.success){
@@ -43,8 +52,12 @@ export default {
         }
     },
 
+    components:{
+      addBrand
+    },
+
     created(){
-        // this.query();
+        this.query();
     }
     
 }
